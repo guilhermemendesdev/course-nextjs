c23c003f4d0526ebea7c78ee8abeb138ea863bf3
Guilherme <guilherme.mendes@anbetec.com.br>
2025-02-12 07:06:13 -0300

    feat: linkando varias paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      HOME PAGE
+    <div>
+      <div>
+        <Link href="/performance">Performance</Link>
+        <Link href="/reliability">Reliability</Link>
+        <Link href="/scale">Scale</Link>
+      </div>
     </div>
   );
 }
