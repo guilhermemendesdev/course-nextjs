b21c7307f9f5a23ba04d31426e427443a37c6823
Guilherme <guilherme.mendes@anbetec.com.br>
2025-02-13 06:35:25 -0300

    feat: retira Link da pagina inicial e adiciona em Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
-    </div>
-  );
+  return <div>Home Page</div>;
 }
